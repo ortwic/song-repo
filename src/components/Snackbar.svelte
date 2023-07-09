adfa5d1b9e8869cfc3c6d6e54cbd51867b55f95f
Ortwin <ohc84@gmx.de>
2023-07-09 15:06:09 +0200

    fixed snackbar animation

--- src/components/Snackbar.svelte
+++ src/components/Snackbar.svelte
@@ -1,40 +1,58 @@
 <script lang="ts">
+    import { fade, slide, type SlideParams } from 'svelte/transition';
+    import { cubicOut } from 'svelte/easing';
+
     let snackbar: HTMLDivElement;
     let snackbarMessage = '';
+    let visible = false;
     let className = '';
     let timeoutId: NodeJS.Timeout; 
 
     export const open = (text: string, timeoutMs = 3000) => {
-      className = 'show';
       show(text, timeoutMs);
     };
 
     export const error = (text: string, timeoutMs = 3000) => {
-      className = 'show error';
+      className = 'error';
       show(text, timeoutMs);
     };
 
     export const close = () => {
       snackbarMessage = '';
       className = '';
+      visible = false;
     };
 
     function show(text: string, timoutMs: number) {
+      visible = true;
       snackbarMessage = text;
 
       clearTimeout(timeoutId);
       timeoutId = setTimeout(() => close(), timoutMs);
     }
+
+    const slideFade = (node: Element, { delay = 0, duration = 400 }: SlideParams) => ({
+      delay,
+      duration,
+      css: (t: number, u: number) => {
+        const slideTransition = slide(node, { delay, duration, easing: cubicOut });
+        const fadeTransition = fade(node, { delay, duration, easing: cubicOut });
+        return `${slideTransition.css(t, u)} ${fadeTransition.css(t, u)}`;
+      }
+    })
 </script>
-  
-<div id="snackbar" class={className} bind:this={snackbar}>
+
+{#if visible}
+<div id="snackbar" class={className} 
+  in:slideFade={{duration: 200}} 
+  out:slideFade={{duration: 200}} 
+  bind:this={snackbar}>
   {snackbarMessage}
 </div>
+{/if}
 
-<style>
-   /* The snackbar - position it at the bottom and in the middle of the screen */
+<style lang="scss">
   #snackbar {
-    visibility: hidden; 
     position: fixed; 
     padding: 16px;
     z-index: 1;
@@ -47,35 +65,7 @@
     text-shadow: 1px 1px 3px rgba(0, 0, 0, 1);
   }
 
-  /* Show the snackbar when clicking on a button (class added with JavaScript) */
-  #snackbar.show {
-    visibility: visible; 
-    -webkit-animation: fadein 0.5s, fadeout 0.5s 2.5s;
-    animation: fadein 0.5s, fadeout 0.5s 2.5s;
-  }
-
   #snackbar.error {
     background-color: firebrick;
   }
-
-  /* Animations to fade the snackbar in and out */
-  @-webkit-keyframes fadein {
-    from {bottom: -3rem; opacity: 0;}
-    to {bottom: 0; opacity: 1;}
-  }
-
-  @keyframes fadein {
-    from {bottom: -3rem; opacity: 0;}
-    to {bottom: 0; opacity: 1;}
-  }
-
-  @-webkit-keyframes fadeout {
-    from {bottom: 0; opacity: 1;}
-    to {bottom: -3rem; opacity: 0;}
-  }
-
-  @keyframes fadeout {
-    from {bottom: 0; opacity: 1;}
-    to {bottom: -3rem; opacity: 0;}
-  } 
 </style>
