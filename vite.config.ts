b8e43869c04764368a0cd06015e8389d275419b8
Ortwin <ohc84@gmx.de>
2023-07-20 21:44:23 +0200

    updated packages and eslint

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
-import { svelte } from '@sveltejs/vite-plugin-svelte';
 import { VitePWA } from 'vite-plugin-pwa';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,10 +8,10 @@ export default defineConfig({
         rollupOptions: {
             output: {
                 manualChunks: {
-                    'tabulator-tables': ['tabulator-tables']
-                }
-            }
-        }
+                    'tabulator-tables': ['tabulator-tables'],
+                },
+            },
+        },
     },
     plugins: [
         svelte(),
