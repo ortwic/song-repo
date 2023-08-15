9339e3889efad89d3acf01d8424e3861e23322a4
Ortwin <ohc84@gmx.de>
2023-08-15 18:46:27 +0200

    replaced SearchInput with Autocomplete package

--- src/components/table/AddEntry.svelte
+++ src/components/table/AddEntry.svelte
@@ -1,15 +1,16 @@
 <script lang="ts">
     import '../../styles/search.scss';
     import { onMount } from "svelte";
+    import Autocomplete from 'simple-svelte-autocomplete/src/SimpleAutocomplete.svelte';
     import AddButton from "../ui/AddButton.svelte";
     import ConfirmDialog from "../ui/ConfirmDialog.svelte";
     import Image from "../ui/elements/Image.svelte";
-    import SearchInput from "../ui/SearchInput.svelte";
+    import LoadingBar from '../ui/elements/LoadingBar.svelte';
     import SongService from "../../service/song.service";
     import SearchService, { create } from "../../service/search.service";
     import type { UserSong } from "../../model/song.model";
-    import { showError } from "../../store/notification.store";
     import type { ArtistResult, SongResult } from "../../model/songbpm.model";
+    import genres from '../../data/genres.json';
     
     const required = true;
     const songService = new SongService();
@@ -88,7 +89,7 @@
 
 {#if visible}
 <form bind:this={form} on:submit|preventDefault>
-    <ConfirmDialog size='full' on:closed={done}>
+    <ConfirmDialog size='auto' on:closed={done}>
         <div class="title" slot="title">
             Search - powered by <a href="https://getsongbpm.com/api" target="_blank">GetSongbpm</a>
         </div>
@@ -96,64 +97,71 @@
             <div class="section flex">
                 <div>
                     <label for="artist">Artist</label>
-                    <SearchInput id="artist" placeholder="artist" value={newSong.artist ?? ''} 
-                        {required} debounceMs={500} minChars={2}
-                        let:results request={(value) => searchService.findArtists(value)} 
-                        on:clear={() => setArtist()} on:error={({ detail }) => showError(detail)}>
-                        
-                        {#each results ?? [] as item}
-                        <!-- svelte-ignore a11y-click-events-have-key-events -->
-                        <div class="option" on:click={() => setArtist(item)}>
+                    <Autocomplete inputClassName="lg" labelFieldName="name" {required} placeholder="artist" 
+                        delay={500} minCharactersToSearch={2} hideArrow={true} bind:text={newSong.artist}
+                        searchFunction={(value) => searchService.findArtists(value)}
+                        onChange={(item) => setArtist(item)} clearSelection={() => setArtist()} showClear={true}
+                        showLoadingIndicator={true}>
+                        <div class="option" slot="item" let:item>
                             <Image src={item.img} />
-                            <div>
+                            <div class="col">
                                 <a title="Artist" href={item.uri} target="_blank">{item.name}</a>
                                 <span title="Country">{item.from ?? ''}</span>
-                                <br>
-                                {#each item.genres ?? [] as genre}
-                                <span class='label'>{genre}</span>
-                                {/each}
+                                <p>
+                                    {#each item.genres ?? [] as genre}
+                                    <span class='label'>{genre}</span>
+                                    {/each}
+                                </p>
                             </div>
                         </div>
-                        {/each}
-                    </SearchInput>
+                        <div slot="loading">
+                            <LoadingBar />
+                        </div>
+                    </Autocomplete>
                 </div>
                 <div>
                     <label for="title">Title</label>
-                    <SearchInput id="title" placeholder="title" value={newSong.title ?? ''} 
-                        {required} debounceMs={newSong.artist ? 500 : 990} minChars={newSong.artist ? 0 : 3}
-                        let:results request={(value) => searchService.findSongs(value, newSong.artist)} 
-                        on:clear={() => setSong()} on:error={({ detail }) => showError(detail)}>
-
-                        {#each results ?? [] as item}
-                        <!-- svelte-ignore a11y-click-events-have-key-events -->
-                        <div class="option" on:click={() => setSong(item)}>
+                    <Autocomplete inputClassName="lg" labelFieldName="title" {required} placeholder="title" 
+                        delay={newSong.artist ? 500 : 990} minCharactersToSearch={newSong.artist ? 0 : 3} hideArrow={true}
+                        searchFunction={(value) => searchService.findSongs(value, newSong.artist)} 
+                        onChange={(item) => setSong(item)} clearSelection={() => setSong()} showClear={true}
+                        showLoadingIndicator={true}>
+                        <div class="option" slot="item" let:item>
                             <a title={item.album?.title} href={item.album?.uri ?? item.artist?.uri} target="_blank">
                                 <Image src={item.album?.img ?? item.artist?.img} />
                             </a>
-                            <div>
+                            <div class="col">
                                 <a title="Title" href={item.uri} target="_blank">{item.title}</a>
                                 {#if newSong.artist}
                                 <span title="Year">({item.album?.year})</span>
-                                <br>
-                                <span title="Tempo" class='label'>{item.tempo}</span>
-                                <span title="Time" class='label'>{item.time_sig}</span>
-                                <span title="Key" class='label'>{item.key_of}</span>
+                                <p>
+                                    <span title="Tempo" class='label'>{item.tempo}</span>
+                                    <span title="Time" class='label'>{item.time_sig}</span>
+                                    <span title="Key" class='label'>{item.key_of}</span>
+                                </p>
                                 {:else}
                                 <br>
                                 <a title="Artist" href={item.artist?.uri} target="_blank">{item.artist?.name}</a>
-                                <br>
+                                <p>
                                     {#each item.artist?.genres ?? [] as genre}
                                     <span class='label'>{genre}</span>
                                     {/each}
+                                </p>
                                 {/if}
                             </div>
                         </div>
-                        {/each}
-                    </SearchInput>
+                    </Autocomplete>
                 </div>
                 <div>
                     <label for="genre">Genre</label>
-                    <input id="genre" class="lg" type="text" placeholder="genre" autocapitalize="words" bind:value={newSong.genre}>
+                    <Autocomplete inputClassName="lg" labelFieldName="name" placeholder="genre" minCharactersToSearch={0}
+                        searchFunction="{() => genres}" showClear={true}
+                        bind:text={newSong.genre} hideArrow={true}>
+                        <svelte:fragment slot="item" let:item let:label>
+                            <div class="genre" style:background-color={item.color}></div>
+                            <span class="genre">{@html label}</span>
+                        </svelte:fragment>
+                    </Autocomplete>
                 </div>
                 <div>
                     <label for="style">Styles</label>
@@ -173,14 +181,16 @@
                     </div>
                 </div>
                 <div>
-                    <label for="difficulty">Difficulty</label>
-                    <input id="difficulty" class="sm" type="number" min="1" max="10" bind:value={newSong.difficulty} on:keydown={addLabel}>
                     <label for="bpm">Tempo</label>
                     <input id="bpm" class="sm" type="text" placeholder="120" bind:value={newSong.bpm} on:keydown={addLabel}>
-                    <label for="time">Time</label>
-                    <input id="time" class="sm" type="text" placeholder="4/4" bind:value={newSong.time} on:keydown={addLabel}>
+                </div>
+                <div>
                     <label for="key">Key</label>
                     <input id="key" class="sm" type="text" placeholder="C" bind:value={newSong.key} on:keydown={addLabel}>
+                </div>
+                <div>
+                    <label for="time">Time</label>
+                    <input id="time" class="sm" type="text" placeholder="4/4" bind:value={newSong.time} on:keydown={addLabel}>
                 </div>
             </div>
         </section>
@@ -205,10 +215,22 @@ form {
         }
     }
 
+    div.genre {
+        display: inline-block;
+        width: 1.2em;
+        height: 1.2em;
+    }
+
+    span.genre {
+        padding: .4em;
+    }
+
     section {
         margin: 1em;
+        min-width: 50rem;
         min-height: 50vh;
         height: 100%;
+        overflow-y: auto;
 
         div.flex {
             display: flex;
@@ -217,6 +239,7 @@ form {
             
             & > div {
                 padding: 0;
+                margin-right: 2em;
             }
 
             div.labels {
@@ -235,17 +258,12 @@ form {
             padding: 4px;
             display: flex;
 
-            a, span {
+            a, span, p {
                 white-space: nowrap;
             }
 
-            div {
+            div.col {
                 padding: 0 1em;
-            }
-
-            &:hover {
-                background-color: var(--primselect);
-                transition: all .2s ease-in-out;
             }
         }
     }
